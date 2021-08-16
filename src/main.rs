22bfc65604a2db0ab731b2cf99b8fac1d33db14f
Tristan Ravitch <tristan@ravit.ch>
2021-08-15 21:55:42 -0700

    Add an integration test
    The test builds bitcode for zlib and ensures that the bitcode can be extracted
    from the shared library. It does not ensure that the bitcode is correct yet.
    
    The integration test implementation needs to be refactored to promote code
    reuse, but I'd rather get it working before doing that.
    
    This ended up requiring a few related changes. These could have been extracted
    into smaller individual commits, but it probably isn't worth the effort.  The
    changes are:
    
    - Improved the module structure to expose the logic of `main` as a function that
    can be reused from the tests
    - Fixed the module structure (the original top-level module was incorrect and
    also never built); this includes necessary updates to Cargo.toml
    - Compute an exit code from the top-level build command and propagates it to the
    executable (useful in general, but especially helpful for the test suite to
    detect if the build fails unexpectedly)
    - Modify the extract command to take the path to `llvm-link` directly, rather
    than an executable suffix (this is more flexible, mirrors other tools, and makes
    the test harness simpler)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
-mod bom;
-
 use structopt::StructOpt;
 
-use crate::bom::bitcode::bitcode_entrypoint;
-use crate::bom::extract::extract_bitcode_entrypoint;
-use crate::bom::normalize::normalize_entrypoint;
-use crate::bom::trace::trace_entrypoint;
-use crate::bom::options::{Options,Subcommand};
-
+use bom;
+use bom::bom::options::Options;
 
 fn main() -> anyhow::Result<()> {
     let opt = Options::from_args();
-
-    match opt.subcommand {
-        Subcommand::Trace(trace_opts) => { trace_entrypoint(&trace_opts) }
-        Subcommand::Normalize(normalize_opts) => { normalize_entrypoint(&normalize_opts) }
-        Subcommand::GenerateBitcode(bitcode_opts) => { bitcode_entrypoint(&bitcode_opts) }
-        Subcommand::ExtractBitcode(extract_opts) => { extract_bitcode_entrypoint(&extract_opts) }
-    }
+    let ec = bom::run_bom(opt)?;
+    std::process::exit(ec);
 }
 
 
